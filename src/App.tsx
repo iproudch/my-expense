54b282b163591fd2d1a28031fec0733d54d78ee9
Proud Laicharoen <chitchanok.lcr@gmail.com>
2024-10-21 12:36:25 +0700

    fixe base route path

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/my-expense">
         <Routes>
           <Route path="/*" element={<AppRoutes />} />
         </Routes>
